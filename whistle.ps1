f6dbb14241e1023ecc30adc61b883c496575209b
Aravind Pai <dragondive@outlook.in>
2024-10-31 15:15:34 +0530

    feat: Create a minimal working prototype (#2)
    This commit creates a minimal working prototype of the `whistle`
    utility. This enables the following features:
    
    * With a single command, automatically download WSL2 image, create
      an instance from it, and provision the instance.
    * Define the overarching structure for the provisioning script,
      consisting two phases: first as the root user and then as the
      standard user.
    * Organize the provisioning into setup bundles, further categorized
      into core bundles and optional bundles.
    * Enable automatic installation of VS Code and the VS Code extensions
      specified in each setup bundle.
    * A README document that explains how to use the utility, as well as
      how to contribute to extend it.
    
    fixes: #1

---
+++ whistle.ps1
@@ -0,0 +1,92 @@
+<#
+.SYNOPSIS
+    Download, create and provision an Ubuntu WSL2 instance.
+.DESCRIPTION
+    This script downloads a user-specified Ubuntu WSL2 image, then creates and
+    provisions a WSL2 instance using the downloaded image.
+.PARAMETER WslDistroName
+    User-chosen name for the WSL2 instance to be created.
+
+    Example values: whistleblower, heavens-arena
+    Default: whistleblower
+.PARAMETER ReleaseName
+    Ubuntu release name. For available WSL releases, see the Ubuntu WSL images page at
+    https://cloud-images.ubuntu.com/wsl/
+
+    Example values: noble, jammy
+    Default: noble
+.PARAMETER ReleaseTag
+    Ubuntu release tag. Available release tags are found inside the directory of the
+    specified release on the Ubuntu WSL images page.
+
+    Example values: current, 20241008
+    Default: current
+.PARAMETER ReleaseArch
+    Architecture of the WSL image to be downloaded.
+
+    Example values: amd64, arm64
+    Default: amd64
+.PARAMETER SetupArgs
+    Arguments for the setup script `whistle.bash`.
+    Due to powershell's parsing limitations, this needs to be enclosed in _both_
+    double quotes and single quotes.
+    Use "'-h'" as the argument to see the arguments documentation.
+
+    Example values: "'-h'", "'-b python3'", "'-u dragondive -b copy-ssh-keys'"
+    Default: ""
+#>
+param
+(
+    [Parameter(HelpMessage="User-chosen name for the WSL2 instance to be created.")]
+    [string]$WslDistroName = "whistleblower",
+
+    [Parameter(HelpMessage="Ubuntu release name.")]
+    [string]$ReleaseName = "noble",
+
+    [Parameter(HelpMessage="Ubuntu release tag.")]
+    [string]$ReleaseTag = "current",
+
+    [Parameter(HelpMessage="Architecture of the WSL image to be downloaded.")]
+    [string]$ReleaseArch = "amd64",
+
+    [Parameter(HelpMessage="Arguments for the setup script whistle.bash")]
+    [string]$SetupArgs = ""
+)
+
+$WslUbuntuUrl = "https://cloud-images.ubuntu.com/wsl"
+$ImageName = "ubuntu-$ReleaseName-wsl-$ReleaseArch-ubuntu.rootfs.tar.gz"
+
+$ImageUrl = "$WslUbuntuUrl/$ReleaseName/$ReleaseTag/$ImageName"
+$SaveDir = "$env:LOCALAPPDATA/whistle"
+$ImagePath = "$SaveDir/$ImageName"
+
+[System.IO.Directory]::CreateDirectory($SaveDir) | Out-Null
+if (!(Test-Path($ImagePath)))
+{
+    Write-Host "Downloading WSL Ubuntu image '$ReleaseName' to '$SaveDir'..."
+    (New-Object Net.WebClient).DownloadFile($ImageUrl, $ImagePath)
+}
+else
+{
+    Write-Host "WSL Ubuntu image '$ReleaseName' already exists, skipping download."
+}
+
+$WslPackagesDir = "$env:LOCALAPPDATA/Packages/WSL"
+[System.IO.Directory]::CreateDirectory($WslPackagesDir) | Out-Null
+wsl --import $WslDistroName $WslPackagesDir/$WslDistroName $ImagePath
+if ($LASTEXITCODE -ne 0)
+{
+    throw "Failed to import WSL Ubuntu image '$ReleaseName' to '$WslDistroName'."
+}
+Write-Host "Successfully imported WSL Ubuntu image '$ReleaseName' to '$WslDistroName'."
+
+# Install vscode on Windows because we will use inside WSL2. This is more convenient
+# and flexible than the direct installation inside WSL2.
+winget install --exact --verbose --accept-package-agreements --accept-source-agreements `
+--version 1.93.0 --force <# prevent "upgrade" to 1.94.0 due to this bug: https://github.com/microsoft/vscode/issues/230584 #> `
+--id Microsoft.VisualStudioCode
+
+$ScriptDir = "$env:TEMP"
+Copy-Item -Path $(Join-Path $PWD "whistle.bash") -Destination $ScriptDir
+Set-Location -Path $ScriptDir
+wsl --distribution $WslDistroName -- bash whistle.bash $SetupArgs
